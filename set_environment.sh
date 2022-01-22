90b3bf5e3854c5a7a0bc05fa99a1dc85a58c1763
Frank Pauxberger <frank@pauxberger.de>
2022-01-22 14:04:36 +0100

    Fix handling wlan vs lan conneciton. Tunnel disabled!

--- set_environment.sh
+++ set_environment.sh
@@ -59,6 +59,7 @@ verbose ""
 verbose "config home (W)LAN1"
 verbose "  SSID: $config_home_ssid"
 verbose "config nas"
+verbose "  NAS server name: $config_nas_nasserver"
 verbose "  NAS path to home: $config_nas_homedrive"
 verbose "  Local mountpoint: $config_nas_mountpoint"
 verbose "  NAS user: $config_nas_user"
@@ -98,14 +99,24 @@ if [ -z "$connection" ]
     fi
 fi 
 
-vverbose "entwork switch: >$network<."
+verbose "network switch: >$network<."
 
+# Check if we sit at HOME
+check_home=$(ping -c1 $config_nas_nasserver &>/dev/null)
+if [ $? -eq 0 ]
+  then 
+    home=true
+  else
+    home=false
+fi
+
+###echo "home=$home"
 
 #
 # If @home mount NAS. If not start vpn tunnel first and then mount NAS
 #
 
-if [ "$network == wlan" ] && [ "$ssid" == "$config_home_ssid" ]
+if [ "$home" == "true" ]
   then
     verbose ""
     verbose "--> Seems you are at home!"
@@ -129,69 +140,69 @@ if [ "$network == wlan" ] && [ "$ssid" == "$config_home_ssid" ]
         fi
     fi
   else
-    verbose ""
-    verbose "You are connected to >$ssid<. This requires some more effort to mount your homedrive. Let's get it done..."
-    verbose ""
+    echo ""
+    echo  "--> Looks like you are not at home. This requires some more effort to mount your homedrive. Let's get it done..."
+    echo ""
 
     ### active_con=$(nmcli con | grep "$active_card")
     ### activ_vpn=$(nmcli con | grep "$ssid")
 
     ### if [ "${activ_con}" -a ! "${activ_vpn}" ];
-    if [ "${ssid}" -a ! "${$config_home_ssid}" ];
-      then
-        if [[ $debug -eq 1 ]]
-          then
-            verbose "nmcli con up id '${config_vpn_vconnection}'"
-            exit 0
-          else
-            nmcli con up id "${config_vpn_vconnection}"
-        fi
-    fi
+    ###if [ "${ssid}" -a ! "${$config_home_ssid}" ];
+    ###  then
+    ###    if [[ $debug -eq 1 ]]
+    ###      then
+    ###        verbose "nmcli con up id '${config_vpn_vconnection}'"
+    ###        exit 0
+    ###      else
+    ###        nmcli con up id "${config_vpn_vconnection}"
+    ###    fi
+    ###fi
     # Check if tunnel is active
-    if [ $? -eq 0 ]
-      then
-        verbose ""
-        verbose "Tunnel successfully activated! Mountin NAS..."
-        verbose ""
+    ### if [ $? -eq 0 ]
+###       then
+    ###     verbose ""
+###         verbose "Tunnel successfully activated! Mountin NAS..."
+        ### verbose ""
 
-        sleep 1
+        ### sleep 1
 
-        verbose ""
-        verbose "Finding the IP of my NAS back in Schluckenau..."
-        verbose ""
+        ### verbose ""
+        ### verbose "Finding the IP of my $config_nas_nasserver back at home..."
+        ### verbose ""
         #
         # Quick and dirty hack to get the NAS mounted in a tunneled area - needs some more brain and time to make it nice
         #
-        ipofnas=`nmap -T5 -sP $config_home_network | grep -i $config_nas_shortname | head -1 | awk {'print $6'}`
-        ipofnas=`echo "${ipofnas:1:${#string}-1}"`
+ ###       ipofnas=`nmap -T5 -sP $config_home_network | grep -i $config_nas_shortname | head -1 | awk {'print $6'}`
+ ###       ipofnas=`echo "${ipofnas:1:${#string}-1}"`
 
-        verbose ""
-        verbose "My NAS in Schluckenau has got IP address: >$ipofnas<!"
-        verbose ""
+ ###       verbose ""
+ ###       verbose "My NAS at home has got IP address: >$ipofnas<!"
+ ###       verbose ""
 
 
-        sleep 2
+ ###       sleep 2
 
-        if [[ $debug -eq 1 ]]
-          then 
+###        if [[ $debug -eq 1 ]]
+###          then 
             ### verbose "sudo mount -t cifs -o user=$config_nas_user,password=$config_nas_password,uid=$config_luser_luid,gid=$config_luser_lgid,nounix,vers=2.0 $config_nas_homedrive $config_nas_mountpoint"
-            verbose "sudo mount -t cifs -o user=$config_nas_user,password=$config_nas_password,uid=$config_luser_luid,gid=$config_luser_lgid,nounix,vers=2.0 //$ipofnas/home $config_nas_mountpoint"
-            exit 0
-          else
-            sudo mount -t cifs -o user=$config_nas_user,password=$config_nas_password,uid=$config_luser_luid,gid=$config_luser_lgid,nounix,vers=2.0 //$ipofnas/home $config_nas_mountpoint
-            if [ $? -eq 0 ]
-              then
-                verbose "Done!"
-                exit 0
-              else
-                verbose "Unable to mount home drive! Exiting..."
-                exit 1
-            fi
-            verbose ""
-	    verbose "Tunnel could not be started. Please check that! Exiting..."
-            verbose ""
-            exit 1
-        fi
+###            verbose "sudo mount -t cifs -o user=$config_nas_user,password=$config_nas_password,uid=$config_luser_luid,gid=$config_luser_lgid,nounix,vers=2.0 //$ipofnas/home $config_nas_mountpoint"
+###            exit 0
+###          else
+###            sudo mount -t cifs -o user=$config_nas_user,password=$config_nas_password,uid=$config_luser_luid,gid=$config_luser_lgid,nounix,vers=2.0 //$ipofnas/home $config_nas_mountpoint
+###           if [ $? -eq 0 ]
+###              then
+###                verbose "Done!"
+###                exit 0
+###              else
+###                verbose "Unable to mount home drive! Exiting..."
+###                exit 1
+###            fi
+###            verbose ""
+	    ###verbose "Tunnel could not be started. Please check that! Exiting..."
+ ###           verbose ""
+ ###           exit 1
+ ###       fi
     fi
 fi
 
